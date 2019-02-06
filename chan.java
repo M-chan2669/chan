1cedaae8898e80add92c310f13e71955f694d986
M-chan4899 <47376848+M-chan4899@users.noreply.github.com>
2019-02-06 14:39:08 +0530

    Update chan.java

--- chan.java
+++ chan.java
@@ -1,18 +1,26 @@
+/* package whatever; // don't place package name! */
+
 import java.util.*;
 import java.lang.*;
 import java.io.*;
-
-/* Name of the class has to be "Main" only if the class is public. */
-class Ideone
+class Even
 {
-	public static void main (String[] args) throws java.lang.Exception
-	{
-	int n=9;
-	if(n%2==0)
-	System.out.println("Even");
-	else if(n%2>0)
-	System.out.println("Odd");
-	else
-	System.out.println("invalid");
-	}
+public static void main (String[] args) throws java.lang.Exception
+{
+Scanner s=new Scanner(System.in);
+int a;
+a=s.nextInt();
+if(a%2==0)
+{
+System.out.println("Even");
+}
+else if(a%2<0)
+{
+System.out.println("invalid");
+}
+else
+{
+System.out.println("Odd");
+}
+}
 }
